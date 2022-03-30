6d41789a493a683df83ef166ea3b24a6f1f4a266
zhangzhangcong <zhangcong@shanghaitech.edu.cn>
2022-03-30 16:32:41 +0800

    修改于mac，加了冒泡排序。

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
-using namespace std
+#include<bits/stdc++.h>
+using namespace std;
+void sort(int arr[], int size)
+{
+    int tmp = 0;
+    for(int i = 0; i < size- 1; ++i)
+    {
+        for(int j = 0; j < size - 1 - i; ++j)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
 int main()
 {
-	int arr = {12,4,5,78,8};
+	int arr[] = {12,4,5,78,8};
 	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
 	return 0;
